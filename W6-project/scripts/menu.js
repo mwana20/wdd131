e4bc309d64b833bb58470eefc3d49128038549b1
mwana20 <phillipmwanaweika@gmail.com>
2025-02-19 20:40:44 +0300

    w6

--- W6-project/scripts/menu.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const hamburgerMenu = document.querySelector('.hamburger-menu');
-    const navLinks = document.querySelector('.nav-links');
-
-    hamburgerMenu.addEventListener('click', () => {
-        navLinks.classList.toggle('active');
-    });
-});
--- W6-project/scripts/redirectToBuyMovie.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const buyButtons = document.querySelectorAll('.buy-movie-button');
-    buyButtons.forEach(button => {
-        button.addEventListener('click', () => {
-            window.location.href = 'buyMovie.html';
-        });
-    });
-});
