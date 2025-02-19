b204dcb92b017022d1941bbf2c64d61645cd641b
mwana20 <phillipmwanaweika@gmail.com>
2025-02-19 09:02:25 +0300

    w6

---
+++ W6-project/scripts/script.js
@@ -0,0 +1,9 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const mobileMenuBtn = document.querySelector('.mobile-menu-btn');
+    const navLinks = document.querySelector('.nav-links');
+
+    mobileMenuBtn.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+});
